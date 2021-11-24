26c0496fdc1725a6372e55d34dbe61f18546d2e4
Pedro Ferreira <id.pedroferreira@outlook.com>
2021-11-23 23:37:39 -0300

    create ActiveLinkCompoent

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,21 @@
-import { Link, LinkProps, Text, Icon } from "@chakra-ui/react";
+import { Link as ChakraLink, LinkProps, Text, Icon } from "@chakra-ui/react";
 import { ElementType } from "react";
-
+import { ActiveLink } from "../ActiveLink";
 interface PNavLink extends LinkProps {
   title: string;
   icon: ElementType;
+  href: string;
 }
 
-export function NavLink({ title, icon, ...rest }: PNavLink) {
+export function NavLink({ title, icon, href, ...rest }: PNavLink) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontSize="medium">
-        {title}
-      </Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontSize="medium">
+          {title}
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
