de20e723f3c0920b89c39c9f014b72f3dfa36a03
Pedro Ferreira <id.pedroferreira@outlook.com>
2021-11-23 23:37:51 -0300

    update buttons links

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,6 +12,7 @@ import { Header } from "../../components/Header";
 import { Input } from "../../components/Form/input";
 
 import { Sidebar } from "../../components/Sidebar";
+import Link from "next/link";
 
 export default function CreateUser() {
   return (
@@ -41,7 +42,11 @@ export default function CreateUser() {
           </VStack>
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href={"/users"} passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
