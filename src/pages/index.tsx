4cd2b951b7f2496d1107f34751cc61367151ac1f
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2022-01-11 22:46:52 -0300

    update login form to react-hook-forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Flex, Stack, Button } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import { Input } from "../components/Form/input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().email("E-mail Invalido").required("E-mail obrigátorio"),
+  password: yup.string().required("Senha obrigátorio"),
+});
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { errors, isSubmitting } = formState;
+
+  const handlerSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex width="100vw" height="100vh" align="center" justify="center">
       <Flex
@@ -8,39 +34,34 @@ export default function Home() {
         width="100%"
         maxW={360}
         bg="gray.800"
-        padding={8}
+        p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handlerSignIn)}
       >
         <Stack spacing={4}>
           <Input
             label="E-Mail"
             name="email"
-            id="email"
             type="email"
-            focusBorderColor="pink.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900",
-            }}
-            size="lg"
+            error={errors.email}
+            {...register("email")}
           />
           <Input
             label="Senha"
             name="password"
-            id="password"
             type="password"
-            focusBorderColor="pink.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900",
-            }}
-            size="lg"
+            error={errors.password}
+            {...register("password")}
           />
         </Stack>
-        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+        <Button
+          isLoading={isSubmitting}
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+        >
           Entra
         </Button>
       </Flex>
